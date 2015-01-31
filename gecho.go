a931c0c638bc04051e44f1af8610676132a182a4
Caius Durling <dev@caius.name>
2015-01-31 09:57:32 +0000

    Setup variables for Exec arguments

--- gecho.go
+++ gecho.go
@@ -1,7 +1,13 @@
 package main
 
-imports (
+import (
+	"fmt"
 )
 
 func main() {
+	cmdPath := "/bin/echo"
+	cmdArgs := []string{"Hello", "World"}
+	cmdEnv := []string{}
+
+	fmt.Printf("path: %v\nargs: %v\nenv: %v\n", cmdPath, cmdArgs, cmdEnv)
 }
